f45591d0cc9474598d14d1117f66f5bc01f77edb
Shadowkj1 <keshankkj@gmail.com>
2024-10-27 02:07:28 -0400

    Sprite added to Player Model

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class Bullet : TemporalEntity
 {
 
 //would is be useful to be able to set the damage and speed for
 //buffs or debuffs?
     public float damage { get; set; } = 10f;
     public float speed { get; set; } = 10f;
-   
+
+
+    Queue<Vector3> positionQueue = new Queue<Vector3>();
 
     // Start is called before the first frame update
     void Start()
@@ -26,4 +28,29 @@ public class Bullet : MonoBehaviour
         //Destroy the bullet after 8 seconds
         Destroy(gameObject, 8f);
     }
+
+
+        public override void SetReversedTimeValues()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void AddToQueues()
+    {
+        positionQueue.Enqueue(transform.position);
+    }
+
+    protected override void CullQueues()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void DequeueQueues()
+    {
+        throw new System.NotImplementedException();
+    }
+
+
+
 }
+
